0603fe9d1d3a992f501804129531e775a2037d7b
ashler24 <pawar2467@gmail.com>
2020-11-05 23:26:20 +0530

    Try the rest countries api below. Extract and print the flag url of all the countries in console. Use the html template. https://restcountries.eu/rest/v2/al

--- task01-01-Oct-2020/printFlagUrl.js
+++ task01-01-Oct-2020/printFlagUrl.js
@@ -3,6 +3,5 @@ function getData() {
     fetch("https://restcountries.eu/rest/v2/all")
         .then(res => res.json())
         .then(data => data.forEach(item => { console.log(item.flag) }))
-        .then(data => console.log(data))
         .catch(err => console.log(err))
 }
